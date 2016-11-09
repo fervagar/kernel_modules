8450b238b497abc618a97de20c45cdf1fa4d8b70
fervagar <fervagar@tuta.io>
2016-11-09 12:09:47 +0100

    Updated email

--- mutex.c
+++ mutex.c
@@ -5,13 +5,33 @@
 #  define MODULE
 #endif
 
+/*
+ * Copyright (C) 2016 Fernando Vanyo Garcia
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *	Fernando Vanyo Garcia <fernando@fervagar.com>
+ */
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 //#include <linux/init.h>
 //#include <linux/syscalls.h>
 #include <linux/kthread.h>
 
-#define AUTHOR "Fernando Vanyo <fervagar@tuta.io>"
+#define AUTHOR "Fernando Vanyo <fernando@fervagar.com>"
 #define DESC   "Simple \"Hello World\" of mutex in kernel"
 
 MODULE_LICENSE("Dual BSD/GPL");
